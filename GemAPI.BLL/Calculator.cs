4f859a765217c87609bb26f8490e8e46449e5f29
thierryns <tnsaka@trakiro.com>
2021-12-20 14:12:02 +0100

    Error handling

--- GemAPI.BLL/Calculator.cs
+++ GemAPI.BLL/Calculator.cs
@@ -19,53 +19,62 @@ namespace GemAPI.BLL
         {
             var target = payload.Load;
             var results = new List<CandidatePlant>();
-            var orderedListByCost = payload.Powerplants
+            try
+            {
+                var orderedListByCost = payload.Powerplants
                 .OrderBy(p => p.ComputeCost(payload.Fuels))
                 .ThenByDescending(p => p.GetRealPower(payload.Fuels))
                 .ToArray();
-            for (int i = 0; i < orderedListByCost.Length; i++)
-            {
-                var item = orderedListByCost[i];
-                var realPower = item.GetRealPower(payload.Fuels);
-                var remainder = target - realPower;
-
-                if (remainder > 0)
+                for (int i = 0; i < orderedListByCost.Length; i++)
                 {
-                    target -= realPower;
-                    results.Add(new CandidatePlant
-                    {
-                        Name = item.Name,
-                        Power = realPower
-                    });
+                    var item = orderedListByCost[i];
+                    var realPower = item.GetRealPower(payload.Fuels);
+                    var remainder = target - realPower;
 
-                }
-                else
-                {
-
-                    var o = new CandidatePlant
+                    if (remainder > 0)
                     {
-                        Name = item.Name,
-                        Power = target
-                    };
-                    results.Add(o);
+                        target -= realPower;
+                        results.Add(new CandidatePlant
+                        {
+                            Name = item.Name,
+                            Power = realPower
+                        });
 
-                    if (target < item.PMin)
-                    {
-                        //ADJUST
-                        var delta = item.PMin - target;
-                        o.Power += delta;
-                        //ADJUST PREVIOUS
-                        var n = results.Count-2;
-                        var previous = results[n];
-                        previous.Power -= delta;
                     }
-                    
-                    break;
+                    else
+                    {
+
+                        var o = new CandidatePlant
+                        {
+                            Name = item.Name,
+                            Power = target
+                        };
+                        results.Add(o);
+
+                        if (target < item.PMin)
+                        {
+                            //ADJUST
+                            var delta = item.PMin - target;
+                            o.Power += delta;
+                            //ADJUST PREVIOUS
+                            var n = results.Count - 2;
+                            var previous = results[n];
+                            previous.Power -= delta;
+                        }
+
+                        break;
+                    }
                 }
+
+                //var orderByPower = orderedListByCost;
+                _logger.LogInformation(2, "OPT Results {@results}", results);
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError(exc, exc.Message);
+                throw;
             }
             
-            //var orderByPower = orderedListByCost;
-            _logger.LogInformation(2, "OPT Results {@results}", results);
             return results.ToArray();
         }
 
